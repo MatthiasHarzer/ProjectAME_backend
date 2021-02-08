3e9b50294e5752d28680758fa528aa2d36718edc
Matthias Harzer <matthias.harzer03@gmail.com>
2021-02-08 14:42:29 +0100

    Users

--- User.java
+++ User.java
@@ -1,4 +1,13 @@
 package server;
 
+import org.java_websocket.WebSocket;
+
 public class User {
+    private String id;
+    private String ip;
+    private String sharecode;
+    private String name;
+    private WebSocket connection;
+
+
 }
